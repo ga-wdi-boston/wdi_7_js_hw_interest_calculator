89989eed290eaf67e93b7b74606865948c463b45
Abby Howell <abby.g.howell@gmail.com>
2013-10-28 17:05:53 -0400

    done

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,14 @@
  
+
+var total_amount_borrowed, interest_rate, years, total_interest, total_amount;
+
+total_amount_borrowed = prompt("Total amount borrowed?");
+interest_rate = prompt("Interest rate in percent");
+years = prompt("How many years?");
+
+total_interest = total_amount_borrowed * (interest_rate / 100) * years
+total_amount = total_interest + total_amount_borrowed
+
+console.log("The total interest accumulated will be $" + total_interest + ".00");
+
+console.log("You will pay back " + total_amount + "over the course of the loan." )
