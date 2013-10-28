3bcd3ca2347e3af0f7bd37680c737f9b7d991f79
brycelambert <brycelambert@gmail.com>
2013-10-28 16:55:14 -0400

    finished hw

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,15 @@
- 
+var p, r, n, fv;
+
+p = prompt("Whats the principal?");
+r = prompt("Whats the interest rate?");
+n = prompt("How long will the $$ be in the bank?");
+
+p = parseFloat(p);
+r = parseFloat(r);
+r = r/100;
+n = parseFloat(n);
+
+fv = Math.pow((p * (1 + r)), n);
+fv = fv.toFixed(2);
+
+document.write("The Future Value of your investment is: $" + fv);
