37c2e11402a8522881034d7167da7e1c5a8cbd9f
Matt Clement <darkmoves@gmail.com>
2013-10-28 17:11:41 -0400

    Added high_low.js to index.html and coded most of it up.

--- high_low.js
+++ high_low.js
@@ -1,1 +1,16 @@
- 
+var x, guess;
+
+x = 5; 
+guess = prompt("Guess the number betwen 1 and 10.")
+
+if (x < guess) {
+	console.log("Too high.");
+}
+else if (guess < x) {
+	console.log("Too low.");
+}
+else if (guess === x) {
+	console.log("That's correct.");
+}
+
+
