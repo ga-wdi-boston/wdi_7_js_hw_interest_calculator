7b53af3c1d80c1dfab1df395f9ce8bc225c31f58
Michael Finneran <mrfinneran@gmail.com>
2013-10-28 22:33:03 -0400

    completed assignment

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,7 @@
- 
+var rate, borrowed, years;
+rate = prompt("interest rate?"); 
+borrowed = prompt("amount borrowed?"); 
+years = prompt("years?"); 
+
+interest = parseFloat(rate) * parseFloat(borrowed) * parseFloat(years)
+console.log(interest)
