505e143ba1834a080ca0b4b4d152f1260fa8687a
Abby Howell <abby.g.howell@gmail.com>
2013-10-29 09:23:33 -0400

    fixed errors after parseFloat discussion in class

--- interest_calc.js
+++ interest_calc.js
@@ -2,13 +2,13 @@
 
 var total_amount_borrowed, interest_rate, years, total_interest, total_amount;
 
-total_amount_borrowed = prompt("Total amount borrowed?");
-interest_rate = prompt("Interest rate in percent");
-years = prompt("How many years?");
+total_amount_borrowed = parseFloat(prompt("Total amount borrowed?"));
+interest_rate = parseFloat(prompt("Interest rate in percent"));
+years = parseFloat(prompt("How many years?"));
 
 total_interest = total_amount_borrowed * (interest_rate / 100) * years
 total_amount = total_interest + total_amount_borrowed
 
 console.log("The total interest accumulated will be $" + total_interest + ".00");
 
-console.log("You will pay back " + total_amount + "over the course of the loan." )
+console.log("You will pay back $" + total_amount + ".00 over the course of the loan." )
