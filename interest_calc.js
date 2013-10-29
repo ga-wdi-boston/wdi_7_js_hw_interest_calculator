7c49e7e7a507f6502cf94f86dd4f757bd4d3b870
Bobby Mazzotti <robert.mazzotti@gmail.com>
2013-10-28 23:12:47 -0400

    Final result of jscript homework

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,8 @@
- 
+var total_amount, interest_amount, years, interest;
+total_amount = prompt("What's the total amout borrowed?");
+interest_rate = prompt("What's the total interest rate (%)?");
+years = prompt("How many years is the loan for?");
+
+interest = total_amount * (interest_amount / 100) * years;
+
+document.write(interest)
