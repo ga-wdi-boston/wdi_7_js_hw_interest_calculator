426c4914feb3c1c416bd25492bd30bbd60225a55
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-28 22:50:52 -0400

    homework done

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,8 @@
- 
+var total_amount, interest_rate, years, interest;
+total_amount = prompt("What's the total amout borrowed?");
+interest_rate = prompt("What's the interest rate (%)?");
+years = prompt("How many years is the loan for?");
+
+interest = total_amount * (interest_rate / 100) * years;
+
+document.write(interest)
