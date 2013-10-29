3d7c58713cbcd0cf50021c32e916d388459be0be
Matt Clement <darkmoves@gmail.com>
2013-10-28 20:25:20 -0400

    Complete logic for high/low game.

--- high_low.js
+++ high_low.js
@@ -1,16 +1,17 @@
 var x, guess;
 
-x = 5; 
-guess = prompt("Guess the number betwen 1 and 10.")
+x = Math.floor(Math.random() * 10) + 1; 
+guess = parseInt(prompt("Guess the number betwen 1 and 10."));
 
-if (x < guess) {
-	console.log("Too high.");
+
+if (guess === x) {
+	console.log("That's correct.");
 }
 else if (guess < x) {
-	console.log("Too low.");
+	console.log("Too low. The number is " + x + ".");
 }
-else if (guess === x) {
-	console.log("That's correct.");
+else if (x < guess) {
+	console.log("Too high. The number is " + x + ".");
 }
 
 
