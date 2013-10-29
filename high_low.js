00bc5de2a5781da5d8c634f38bbac3dd7ec01977
Rahul Horé <hore.rahul@gmail.com>
2013-10-29 00:33:41 -0400

    Implements guessing game

--- high_low.js
+++ high_low.js
@@ -1,1 +1,15 @@
- 
+var guess = 0, the_number = Math.floor((Math.random()*10)+1);
+do {
+  guess = prompt("Guess a number between 1 and 10:");
+  if (guess > the_number) {
+    console.log("Too high.");
+  } 
+  else if (guess < the_number) {
+    console.log("Too low.");
+  }
+  else {
+    console.log("You guessed the number! How did you know?");
+    break;
+  }
+}
+while (guess !== the_number)
