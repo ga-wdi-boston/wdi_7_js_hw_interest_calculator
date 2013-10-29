f626609cb87ee259ac73e1deb993d3ae7c842225
Dan Crews <dancrews@me.com>
2013-10-29 09:35:46 -0400

    HW Submission

---
+++ :: main.js
@@ -0,0 +1,14 @@
+// main.js
+//simple interest calculator
+
+
+var principal = prompt("What is the principal?");
+var term = prompt("What is the term of the loan?");
+var interest-rate = prompt("What is the interest rate?");
+var interest = 0;
+
+interest = ((balance * interest-rate * term)/100);
+
+alert("Your interest payment will be: $" interest)
+
+
