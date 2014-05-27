7bf3ed1be204fcdae82fe70ae6ed0a978718d563
Ethan Gould <ethan.g.907@gmail.com>
2014-05-27 17:31:35 -0400

    completed interest rate calculator!

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,7 @@
- 
+var borrowed = prompt("amount to be borrow?");
+var interest_rate = prompt("At what interest rate?");
+var years = prompt("How many years to pay off?");
+
+var cost = borrowed * interest_rate * years;
+
+alert("you will owe $" + cost);
