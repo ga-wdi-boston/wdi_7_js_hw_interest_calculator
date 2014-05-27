c6b02e4943754b7cb5882d1db18df76ce701af5f
Lisa Plesko <plesko.l@gmail.com>
2014-05-27 17:53:52 -0400

    Solution for interest calculator

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,9 @@
- 
+var borrowed = parseInt(prompt('Amount borrowed?'));
+var rate = parseInt(prompt('Interest rate?'));
+var years = parseInt(prompt('Number of years?'));
+
+function interest_rate(borrowed, rate, years) {
+  return ((borrowed * (rate/100) * years)).toFixed(2);
+}
+
+console.log('You owe $' + interest_rate(borrowed, rate, years) + ' immediately!');
