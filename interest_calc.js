325e0052fb9c9f6c332fc27a0073e56bd43ba542
Mike Talley <miketalley@hotmail.com>
2014-05-27 17:21:32 -0400

    Added variable prompts for interest calculator

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,5 @@
- 
+var amount_borrowed = 250000 // prompt('Enter Amount Borrowed(ex - 250000): ');
+var interest_rate = 2.75 // prompt('Enter Interest Rate(ex - 3.75): ');
+var num_years = 30 // prompt('Enter Number of Years for Loan(ex - 30): ');
+
+
