b334a66de86b7f93b58d92dd0141e3122b0bea69
Christine Lafontant <Christine@Christines-MacBook-Pro.local>
2014-05-27 20:34:31 -0400

    interest calculator completed and functional

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,13 @@
- 
+function loanCalc () {
+	// Ask for principal
+	var principal = prompt("Enter the principal:");
+	// Ask for interest
+	var interest = prompt("Enter annual interest rate:");
+	// Ask for number of years
+	var time = prompt("Enter the number of years of the loan term:");
+	// Provide user with loan interest total
+	alert("Total loan interest is" + (principal * interest * time))
+}
+
+
+
