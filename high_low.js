3685c34d0b0e4382ec96e65be1f962a88934fa86
Ethan Gould <ethan.g.907@gmail.com>
2014-05-28 07:08:16 -0400

    user exites the game upon correct guess

--- high_low.js
+++ high_low.js
@@ -5,10 +5,12 @@ function numberGuess(){
   var correct_num = randomNumber(1,10);
   var chances = 3;
   var guess;
-  while(guess !== correct_num && chances > 0){
+  console.log('correct number: ' + correct_num);
+  while(guess != correct_num && chances > 0){
     guess = prompt("Guess a number between 1 and 10");
     if(guess == correct_num){
       alert('You got it!');
+      chances = 0;
     } else if(guess < correct_num){
       alert('too low, try again');
     } else if(guess > 10){
@@ -18,9 +20,12 @@ function numberGuess(){
       alert('too high, guess again');
     }
     chances -= 1;
+    console.log(chances);
   }
-  if(chances < 1 && guess !== correct_num){
+  if(guess != correct_num){
     alert('You are out of guesses, the correct number was ' + correct_num);
+  } else if(guess == correct_num) {
+    alert('You win!');
   }
 }
 
