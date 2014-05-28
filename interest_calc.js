3a188942ca796ffdf606d3a2cf0df7f780720a72
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-27 21:25:12 -0400

    Finished interest calculator

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,21 @@
- 
+// 1. Write a simple interest calculator for loans.
+// Ask the user for the amount borrowed, interest rate, and number of years,
+// and output the amount of interest that will be paid.
+
+// var amountBorrowed = prompt("How much was borrowed?");
+// var interestRate = prompt("What is the interest rate?");
+// var numberOfYears = prompt("Number of years");
+
+var amountBorrowed = 600.00;
+var interestRate = 0.15;
+var numberOfYears = 5;
+
+function calculateInterest(amount, rate, years) {
+  for(var i = 0; i <= years; i++){
+    var interest = (amount*rate);
+    amount += interest;
+  }
+  console.log(amount);
+}
+
+calculateInterest(amountBorrowed, interestRate, numberOfYears);
