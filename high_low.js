2aaf646225c6b9ee8f789200f48e4c14a1b6f577
George Straehle <gstraehle@gmail.com>
2014-05-28 01:36:41 -0400

    correctly counts number of guesses

--- high_low.js
+++ high_low.js
@@ -5,9 +5,14 @@ var random_number = Math.floor((Math.random() * 10) + 1),
 while (guess != random_number) {
   if (guess - random_number > 0) {
     guess = prompt("guess lower");
+    guess_count++;
   }
   else {
-    guess = prompt("guss higher");
+    guess = prompt("guess higher");
+    guess_count++;
   }
 }
-alert("you got it, the number was " + random_number);
+
+alert("you got it in " + guess_count + " tries, the number was " + random_number);
+
+
