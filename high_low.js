9759826cf7348ea3c2f4d10db9ababc6c32eabd0
Ethan Gould <ethan.g.907@gmail.com>
2014-05-27 21:24:02 -0400

    accounted for out of range guesses

--- high_low.js
+++ high_low.js
@@ -5,19 +5,23 @@ function numberGuess(){
   var correct_num = randomNumber(1,10);
   var chances = 3;
   var guess;
-  alert(correct_num);
   while(guess !== correct_num && chances > 0){
     guess = prompt("Guess a number between 1 and 10");
-    if(guess === correct_num){
+    if(guess == correct_num){
       alert('You got it!');
     } else if(guess < correct_num){
       alert('too low, try again');
+    } else if(guess > 10){
+      alert('Out of range! try a number less than 10');
+      chances += 1;
     } else if(guess > correct_num) {
       alert('too high, guess again');
     }
     chances -= 1;
   }
-  alert('You are out of guess, the correct number was ' + correct_num);
+  if(chances < 1 && guess !== correct_num){
+    alert('You are out of guesses, the correct number was ' + correct_num);
+  }
 }
 
 numberGuess();
