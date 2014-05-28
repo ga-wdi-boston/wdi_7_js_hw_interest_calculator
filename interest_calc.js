85be94ad4a0cf7007e8886e1505215d958ce29c8
Ethan Gould <ethan.g.907@gmail.com>
2014-05-28 07:09:25 -0400

    made user values pass in to function

--- interest_calc.js
+++ interest_calc.js
@@ -1,10 +1,10 @@
-function intrestRate() {
-
-  var borrowed = prompt("amount to be borrow?");
-  var interest_rate = prompt("At what interest rate?");
-  var years = prompt("How many years to pay off?");
+function intrestRate(borrowed, interest_rate, years) {
   var cost = borrowed * interest_rate * years;
-
   alert("you will owe $" + cost + ' in interest');
 }
-intrestRate();
+
+var borrowed = prompt("amount to be borrow?");
+var interest_rate = prompt("At what interest rate?");
+var years = prompt("How many years to pay off?");
+
+intrestRate(borrowed, interest_rate, years);
