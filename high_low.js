e8175fc3c688b5552147a6246d3bc0e197c76af4
George Straehle <gstraehle@gmail.com>
2014-05-28 01:10:35 -0400

    created prompts with logic while loop
    while loop in permenant loop mode....

--- high_low.js
+++ high_low.js
@@ -1,2 +1,13 @@
-var random_number = Math.floor((Math.random() * 10) + 1);
+var random_number = Math.floor((Math.random() * 10) + 1),
+    guess = prompt("Make a guess")
+    guess_count = 1;
 
+while (guess !== random_number) {
+  if (guess - random_number > 0) {
+    guess = prompt("guess lower");
+  }
+  else {
+    guess = prompt("guss higher");
+  }
+}
+alert("you got it, the number was " + random_number);
