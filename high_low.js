6cea3dd4e1b2933e34bddcb9ce53f0e78d47b356
Mike Talley <miketalley@hotmail.com>
2014-05-27 20:20:58 -0400

    Added framework for high_low
    * Prompts user to play
    * Generates random number from 1-10

--- high_low.js
+++ high_low.js
@@ -1,1 +1,11 @@
- 
+var guessed = false;
+var guess = 0;
+var playing = false;
+
+playing = confirm('Would you like to play a game?');
+
+while(playing) {
+  var the_number = Math.floor((Math.random() * 10) + 1);
+  alert(the_number);
+  playing = confirm('Would you like to play again?');
+}
