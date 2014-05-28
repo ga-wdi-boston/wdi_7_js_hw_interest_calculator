5c0a7fd1c99215bc9d957d183fef7fd5a477f0ec
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-27 21:17:32 -0400

    Create prompts for interest calculator

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,12 @@
- 
+// promt to ask the user for amount borrowed
+var loanAmount = prompt("How much would you like to borrow?");
+
+// prompt to ask the user for interest rate
+
+var interest = prompt("What is the interest rate?");
+
+// prompt to ask the user for number of years
+
+var years = prompt("How many years to pay back this loan?");
+
+// calculation that will return the amount of interest that will be paid
