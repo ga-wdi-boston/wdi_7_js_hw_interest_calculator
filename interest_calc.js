af7994a97fdb8ac55db86fa092a2bb48bceae048
Mike Talley <miketalley@hotmail.com>
2014-05-27 20:06:55 -0400

    Added code for interest calculation

--- interest_calc.js
+++ interest_calc.js
@@ -1,5 +1,9 @@
-var amount_borrowed = 250000 // prompt('Enter Amount Borrowed(ex - 250000): ');
-var interest_rate = 2.75 // prompt('Enter Interest Rate(ex - 3.75): ');
-var num_years = 30 // prompt('Enter Number of Years for Loan(ex - 30): ');
+var amount_borrowed = 250000; // prompt('Enter Amount Borrowed(ex - 250000): ');
+var interest_rate = 2.75;  // prompt('Enter Interest Rate(ex - 3.75): ');
+var num_years = 30;  // prompt('Enter Number of Years for Loan(ex - 30): ');
 
+var calculateInterest = (function(principal, rate, years) {
+  return principal * (rate * 0.01) * years;
+});
 
+console.log(calculateInterest(amount_borrowed, interest_rate, num_years));
