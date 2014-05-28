d90e1f0ab0ffdf45be690f7333eb206f7aa62d78
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-27 23:26:48 -0400

    Add rest of game

--- high_low.js
+++ high_low.js
@@ -1,19 +1,33 @@
-// prompt to ask for a random number between 1 and 10
-var guess = prompt("Guess a number between 1 and 10.");
-
 // generate a random number between 1 and 10
 var randomNumber = Math.ceil(Math.random()*10);
 
 // function that gives user a hint and tells them the number after three guesses
 
-if(guess > randomNumber) {
-    console.log("Try going a bit lower.");
+var guesses = 3;
+
+console.log(randomNumber);
+
+var guess = -1;
+
+  while (guesses > 0 && guess !== randomNumber) {
+    console.log("You have " + guesses + " guesses remaining.");
+    guess = parseInt(prompt("Guess a number between 1 and 10."));
+    guesses = guesses - 1;
+    if (guesses > 0){
+      if(guess > randomNumber) {
+        console.log("Try going a bit lower.");
+      }
+      else if(guess < randomNumber) {
+        console.log("Try going a bit higher.");
+      }
+    }
   }
-else if(guess < randomNumber) {
-  console.log("Try going a bit higher.");
-}
-else {
-  console.log("Correct!");
-}
+
+  if(guess === randomNumber) {
+    console.log("Correct!");
+  }
+  else {
+    console.log("You are out of guesses. The correct answer was " + randomNumber + ".");
+  }
 
 
