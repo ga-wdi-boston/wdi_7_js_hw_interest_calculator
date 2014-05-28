acd8404ab9c260f11ec4ed259e650b852a649317
George Straehle <gstraehle@gmail.com>
2014-05-27 22:32:11 -0400

    Added prompts for interest calc, tested

--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,25 @@
- 
+console.log("Hello from interest_calc");
+
+var total_borrowed = prompt("How much $$ are you borrowing"),
+    interest_rate = prompt("What is the current rate?"),
+    loan_maturity = prompt("What is the term length (in years)?");
+/*
+function totalInterest (total_borrowed, interest_rate, loan_maturity) {
+  var annual_principle_decrement = (total_borrowed / loan_maturity) /2;
+  return annual_principle_decrement;
+}
+
+*/
+
+
+//alert("You will pay " + total_interest + ".");
+
+/*
+var factorial = (
+  function(x) {
+    if(x <= 1) {
+      return x;
+    }
+  }
+);
+*/
