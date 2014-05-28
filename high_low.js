aa8beffe06c74a33a2f2c8cd5735149afed801f5
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-27 21:58:06 -0400

    Create function for randomly generating a number

--- high_low.js
+++ high_low.js
@@ -1,1 +1,9 @@
- 
+// prompt to ask for a random number between 1 and 10
+
+// generate a random number between 1 and 10
+
+var randomNumber = Math.ceil(Math.random()*10);
+
+  console.log(randomNumber);
+
+
