48a1ce7de01c30338140e6fc08e9e20271ac22de
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-27 22:05:55 -0400

    Add logic to tell user whether guess is correct or not.

--- high_low.js
+++ high_low.js
@@ -1,9 +1,14 @@
 // prompt to ask for a random number between 1 and 10
+var guess = prompt("Guess a number between 1 and 10.");
 
 // generate a random number between 1 and 10
-
 var randomNumber = Math.ceil(Math.random()*10);
 
-  console.log(randomNumber);
+// function that gives user a hint and tells them the number after three guesses
+if(guess === randomNumber) {
+    console.log("Correct!");
+  } else {
+    console.log("Try Again.");
+  }
 
 
