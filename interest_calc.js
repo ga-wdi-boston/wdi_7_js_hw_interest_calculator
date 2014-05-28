878c1376d7b0bc7d6e59a17eb807a11f7d9b91ad
George Straehle <gstraehle@gmail.com>
2014-05-28 00:27:51 -0400

    Calculated interest with while statement

--- interest_calc.js
+++ interest_calc.js
@@ -5,12 +5,18 @@ var total_borrowed = prompt("How much $$ are you borrowing"),
     loan_maturity = prompt("What is the term length (in years)?");
 
 function totalInterest(total_borrowed, interest_rate, loan_maturity) {
-  var annual_principle_decrement = (total_borrowed / loan_maturity) / 2;
-  return annual_principle_decrement;
+  var adjusted_loan_base = total_borrowed - ((total_borrowed / loan_maturity) / 2),
+  interest_paid = 0;
+  for(var loan_year = loan_maturity; loan_year > 0; loan_year--){
+    interest_paid += adjusted_loan_base * interest_rate;
+    adjusted_loan_base -= (total_borrowed / loan_maturity);
+    }
+  return interest_paid
 }
 
 alert("Total interest paid is " + totalInterest(total_borrowed, interest_rate, loan_maturity) + " dollars!");
 
+//for(var i = 0; i < 10; i++)
 
 //alert("You will pay " + total_interest + ".");
 /*
