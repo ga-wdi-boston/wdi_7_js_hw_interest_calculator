28c1ff5b9c0424ca6e0527630171ab4109b2141c
Ethan Gould <ethan.g.907@gmail.com>
2014-05-27 21:34:33 -0400

    updates original to be a function

--- interest_calc.js
+++ interest_calc.js
@@ -1,7 +1,10 @@
-var borrowed = prompt("amount to be borrow?");
-var interest_rate = prompt("At what interest rate?");
-var years = prompt("How many years to pay off?");
+function intrestRate() {
 
-var cost = borrowed * interest_rate * years;
+  var borrowed = prompt("amount to be borrow?");
+  var interest_rate = prompt("At what interest rate?");
+  var years = prompt("How many years to pay off?");
+  var cost = borrowed * interest_rate * years;
 
-alert("you will owe $" + cost);
+  alert("you will owe $" + cost + ' in interest');
+}
+intrestRate();
